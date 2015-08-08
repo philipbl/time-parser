0078b41a884dd209d27aa0e805c94398b0cd0fac
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-07 21:00:52 -0400

    Add more tests

--- tests.rkt
+++ tests.rkt
@@ -8,6 +8,7 @@
 (check-equal? (run-parser/string "6 minutes") "0.1 hours\n")
 (check-equal? (run-parser/string "6 minute") "0.1 hours\n")
 (check-equal? (run-parser/string "6 min") "0.1 hours\n")
+(check-equal? (run-parser/string "6 mins") "0.1 hours\n")
 (check-equal? (run-parser/string "6 m") "0.1 hours\n")
 
 ;; Different hour variations
@@ -21,10 +22,23 @@
 (check-equal? (run-parser/string "120 minutes") "2.0 hours\n")
 
 ;;;;;;;; Absolute time tests
+(check-equal? (run-parser/string "9:00 AM to 5:00 PM") "8.0 hours\n")
 (check-equal? (run-parser/string "12:00 AM to 12:00 AM") "0.0 hours\n")
-
+(check-equal? (run-parser/string "12:00 AM to 12:00 PM") "12.0 hours\n")
+(check-equal? (run-parser/string "12:00 AM to 11:59 PM") "23.983333333333334 hours\n")
+(check-equal? (run-parser/string "9:00 PM to 5:00 PM") "20.0 hours\n")
+; Bug fix
+(check-equal? (run-parser/string "12:00 PM to 5:00 PM") "5.0 hours\n")
 
 ;;;;;;;; Combined tests
+(check-equal? (run-parser/string "9:00 AM to 5:00 PM + 1 hour") "9.0 hours\n")
+(check-equal? (run-parser/string "9:00 AM to 5:00 PM - 1 hour") "7.0 hours\n")
+(check-equal? (run-parser/string "30 minutes + 9:00 AM to 5:00 PM - 1 hour") "7.5 hours\n")
+(check-equal? (run-parser/string "30 minutes - 9:00 AM to 5:00 PM") "-7.5 hours\n")
 
+; Bug
+#| (check-equal? (run-parser/string "9:00 AM to 5:00 PM - 1 hour + 1 hour") "8.0 hours\n") |#
+#| (check-equal? (run-parser/string "8 hours - 5:00 PM to 6:00 PM + 1 hour") "8.0 hours\n") |#
+#| (check-equal? (run-parser/string "8 hours - 5:00 PM to 6:00 PM - 1 hour") "6.0 hours\n") |#
 
 ;;;;;;;; Error tests
