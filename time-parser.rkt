949394e44cec3685031c18198fb081b61ceab6d6
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-07 20:59:48 -0400

    Allow for min and mins

--- time-parser.rkt
+++ time-parser.rkt
@@ -5,7 +5,7 @@
          (prefix-in : parser-tools/lex-sre)
          parser-tools/yacc)
 
-(provide run-parser/port 
+(provide run-parser/port
          run-parser/string)
 
 ; <STATEMENT> : <ABS_STATEMENT> (<OPERATOR> <STATEMENT>)*
@@ -42,17 +42,17 @@
                       (:: #\0 (:/ #\1 #\9))
                       (:/ #\1 #\9))]
   [minute        (:: (:/ #\0 #\5) (:/ #\0 #\9))]
-  
+
   [day           (:or "AM" "PM")]
-  
+
   [joiner        "to"]
-  
+
   [rel-time-num  (:: (:+ digit) (:? rel-time-frac))]
   [rel-time-frac (:: "." (:* digit))]
   [digit         (:/ #\0 #\9)]
-  
-  [rel-time-des  (:or "minutes" "mins" "min" "m" "hours" "hour" "h")]
-  
+
+  [rel-time-des  (:or "minutes" "minute" "mins" "min" "m" "hours" "hour" "h")]
+
   [operator      (:or #\+ #\-)])
 
 (define main-lexer
@@ -79,7 +79,7 @@
   (define amount-num (string->number amount))
   (match type
     [(or "hours" "hour" "h")     (* amount-num 60)]
-    [(or "minutes" "min" "m")    amount-num]))
+    [(or "minutes" "minute" "min" "mins" "m")    amount-num]))
 
 (define (abs-time->minutes time1 am/pm1 time2 am/pm2)
   (define (convert-time time)
@@ -89,12 +89,12 @@
       (if (= hour 12)
           min
           (+ (* hour 60) min))))
-  
+
   (define (12->24 time am/pm)
     (match (string-downcase am/pm)
       ["am"     time]
       ["pm"     (+ time 720)]))
-  
+
   (let* [(begin (12->24 (convert-time time1) am/pm1))
          (end   (12->24 (convert-time time2) am/pm2))
          (diff (- end begin))]
@@ -117,32 +117,32 @@
       (format "~a hours\n" hours)))
 
 (define parse
-  (parser   
-   [grammar 
+  (parser
+   [grammar
     (start     [(statement)  (display-time $1)])
-    
+
     ; <STATEMENT> : <ABS_STATEMENT> (<OPERATOR> <STATEMENT>)*
     ;             | <REL_STATEMENT> (<OPERATOR> <STATEMENT>)*
     (statement [(abs-statement OPERATOR statement) (combine-times $1 $3 $2)]
                [(abs-statement) $1]
                [(rel-statement OPERATOR statement) (combine-times $1 $3 $2)]
                [(rel-statement) $1])
-    
+
     ; <ABS_STATEMENT> : <ABS_TIME> <JOINER> <ABS_TIME>
     (abs-statement [(ABS_TIME DAY JOINER ABS_TIME DAY) (abs-time->minutes $1 $2 $4 $5)])
-    
+
     ; <REL_STATEMENT> : <REL_TIME>
     (rel-statement [(rel-time)  $1])
-    
+
     ; <REL_TIME> : <REL_TIME_NUM> <REL_TIME_DES>
     (rel-time [(REL_TIME_NUM REL_TIME_DES) (rel-time->minutes $1 $2)])]
-   
+
    [tokens tokens empty-tokens]
    [start start]
    [end EOF]
    [error
     (lambda (tok-ok? tok-name tok-value)
-      (raise-syntax-error 
+      (raise-syntax-error
        'parse-error
        (format "~a ~a ~a" tok-ok? tok-name tok-value)))]))
 
@@ -168,7 +168,7 @@
 (define (run-parser/string str)
   (let ([port (open-input-string str)])
     (run-parser/port port)))
-         
+
 
 ;(define arguments (vector->list (current-command-line-arguments)))
 
