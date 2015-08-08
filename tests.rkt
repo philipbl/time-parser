d136420bf6a81cc1f281a737dc9e3802f230091a
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-08 14:48:56 -0400

    Add more tests

--- tests.rkt
+++ tests.rkt
@@ -21,6 +21,10 @@
 (check-equal? (run-parser/string "60 minutes") "1.0 hour\n")
 (check-equal? (run-parser/string "120 minutes") "2.0 hours\n")
 
+;; fraction of times
+(check-equal? (run-parser/string "1.2 h") "1.2 hours\n")
+(check-equal? (run-parser/string "1.2 m") "0.02 hours\n")
+
 ;;;;;;;; Absolute time tests
 (check-equal? (run-parser/string "9:00 AM to 5:00 PM") "8.0 hours\n")
 (check-equal? (run-parser/string "12:00 AM to 12:00 AM") "0.0 hours\n")
@@ -35,10 +39,11 @@
 (check-equal? (run-parser/string "9:00 AM to 5:00 PM - 1 hour") "7.0 hours\n")
 (check-equal? (run-parser/string "30 minutes + 9:00 AM to 5:00 PM - 1 hour") "7.5 hours\n")
 (check-equal? (run-parser/string "30 minutes - 9:00 AM to 5:00 PM") "-7.5 hours\n")
+(check-equal? (run-parser/string "8:00 AM to 11:00 AM + 1:21 PM to 6:05 PM") "7.733333333333333 hours\n")
+(check-equal? (run-parser/string "9:00 AM to 5:00 PM - 12:06 PM to 1:10 PM") "6.933333333333334 hours\n")
 
 ; Bug
 (check-equal? (run-parser/string "9:00 AM to 5:00 PM - 1 hour + 1 hour") "8.0 hours\n")
 (check-equal? (run-parser/string "8 hours - 5:00 PM to 6:00 PM + 1 hour") "8.0 hours\n")
 (check-equal? (run-parser/string "8 hours - 5:00 PM to 6:00 PM - 1 hour") "6.0 hours\n") 
 
-;;;;;;;; Error tests
